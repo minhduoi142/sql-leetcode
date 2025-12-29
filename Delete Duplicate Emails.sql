2408efff5c9fda419d54a1a52839e6294da0b3e7
minhduoi142 <minhduoi142006@gmail.com>
2025-12-30 06:13:56 +0700

    Add files via upload

---
+++ Delete Duplicate Emails.sql
@@ -0,0 +1,18 @@
+-- Write your PostgreSQL query statement below
+delete from Person
+where id in (
+    select id from Person as a  where EXISTS(
+        select * from Person as b
+        where b.email = a.email and b.id < a.id
+    )  
+);
+select * from Person;
+/*
+  __  __  _____  _   _  _   _     _____   _    _   ____   _____ 
+ |  \/  ||_   _|| \ | || | | |   |  __ \ | |  | | / __ \ |_   _|
+ | \  / |  | |  |  \| || |_| |   | |  | || |  | || |  | |  | |  
+ | |\/| |  | |  | . ` ||  _  |   | |  | || |  | || |  | |  | |  
+ | |  | | _| |_ | |\  || | | |   | |__| || |__| || |__| | _| |_ 
+ |_|  |_||_____||_| \_||_| |_|   |_____/  \____/  \____/ |_____|
+           
+*/
